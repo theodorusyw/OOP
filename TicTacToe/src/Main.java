2f522b39d54751f6921ab18548edd6e9b6104556
YourGodzilla <theo.yuriputra19@gmail.com>
2025-01-24 14:24:26 +0700

    #

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
-        char player = 'O';
+        char currPlayer = 'O';
         boolean Won = false;
 
         printBoard();
 
         for (int i = 0; i < 9; i++) {
-            System.out.print("Player " + (player == 'O' ? "1" : "2") + " move : ");
+            System.out.print("currPlayer " + (currPlayer == 'O' ? "1" : "2") + " move : ");
             int x, y;
 
             while (true) {
@@ -23,7 +23,7 @@ public class Main {
                 y = input.nextInt();
 
                 if (x >= 0 && x < 3 && y >= 0 && y < 3 && board [y][x] == '-') {
-                    board[y][x] = player;
+                    board[y][x] = currPlayer;
                     break;
                 } else {
                     System.out.println("Invalid Move");
@@ -32,13 +32,13 @@ public class Main {
 
             printBoard();
 
-            if (checkWin(player)) {
-                System.out.println("Player " + (player == 'O' ? "1" : "2") + " Win!!");
+            if (checkWin(currPlayer)) {
+                System.out.println("currPlayer " + (currPlayer == 'O' ? "1" : "2") + " Win!!");
                 Won = true;
                 break;
             }
 
-            player = (player == 'O') ? 'X' : 'O';
+            currPlayer = (currPlayer == 'O') ? 'X' : 'O';
         }
 
         if (!Won) {
@@ -58,13 +58,13 @@ public class Main {
         System.out.println();
     }
 
-    public static boolean checkWin (char player) {
+    public static boolean checkWin (char currPlayer) {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player || board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+            if (board[i][0] == currPlayer && board[i][1] == currPlayer && board[i][2] == currPlayer || board[0][i] == currPlayer && board[1][i] == currPlayer && board[2][i] == currPlayer) {
                 return  true;
             }
 
-            if (board[0][0] == player && board[1][1] == player && board[2][2] == player || board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            if (board[0][0] == currPlayer && board[1][1] == currPlayer && board[2][2] == currPlayer || board[0][2] == currPlayer && board[1][1] == currPlayer && board[2][0] == currPlayer) {
                 return  true;
             }
         }
