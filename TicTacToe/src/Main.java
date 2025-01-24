f70baf54cdf66e1070d631a03f93e4a872922fed
YourGodzilla <theo.yuriputra19@gmail.com>
2025-01-24 14:23:33 +0700

    #

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
-        char currPlayer = 'O';
+        char player = 'O';
         boolean Won = false;
 
         printBoard();
 
         for (int i = 0; i < 9; i++) {
-            System.out.print("Player " + (currPlayer == 'O' ? "1" : "2") + " move : ");
+            System.out.print("Player " + (player == 'O' ? "1" : "2") + " move : ");
             int x, y;
 
             while (true) {
@@ -23,7 +23,7 @@ public class Main {
                 y = input.nextInt();
 
                 if (x >= 0 && x < 3 && y >= 0 && y < 3 && board [y][x] == '-') {
-                    board[y][x] = currPlayer;
+                    board[y][x] = player;
                     break;
                 } else {
                     System.out.println("Invalid Move");
@@ -32,13 +32,13 @@ public class Main {
 
             printBoard();
 
-            if (checkWin(currPlayer)) {
-                System.out.println("Player " + (currPlayer == 'O' ? "1" : "2") + " Win!!");
+            if (checkWin(player)) {
+                System.out.println("Player " + (player == 'O' ? "1" : "2") + " Win!!");
                 Won = true;
                 break;
             }
 
-            currPlayer = (currPlayer == 'O') ? 'X' : 'O';
+            player = (player == 'O') ? 'X' : 'O';
         }
 
         if (!Won) {
