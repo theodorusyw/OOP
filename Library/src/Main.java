7d00eca0606ada8a941b65d369d5e7725568c081
YourGodzilla <theo.yuriputra19@gmail.com>
2025-02-13 10:14:05 +0700

    #

---
+++ Library/src/Main.java
@@ -0,0 +1,157 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Author {
+    private String name;
+    private String bio;
+
+    public Author(String name, String bio) {
+        this.name = name;
+        this.bio = bio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+}
+
+class Book {
+    private String title;
+    private String isbn;
+    private Author author;
+
+    public Book(String title, String isbn, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+}
+
+class Borrower {
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public boolean returnBook(Book book) {
+        return borrowedBooks.remove(book);
+    }
+
+    public void displayBorrowedBooks() {
+        System.out.println(name + "'s Borrowed Books:");
+        if (borrowedBooks.isEmpty()) {
+            System.out.println("No books borrowed.");
+        } else {
+            for (Book book : borrowedBooks) {
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("ISBN: " + book.getIsbn());
+                System.out.println("Author: " + book.getAuthor().getName());
+                System.out.println("Biography: " + book.getAuthor().getBio());
+                System.out.println("------------------------");
+            }
+        }
+    }
+}
+
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added to library: " + book.getTitle());
+    }
+
+    public void borrowBook(Borrower borrower, Book book) {
+        if (books.contains(book)) {
+            books.remove(book);
+            borrower.borrowBook(book);
+            System.out.println(borrower.getName() + " borrowed: " + book.getTitle());
+        } else {
+            System.out.println("Sorry, " + book.getTitle() + " is not available.");
+        }
+    }
+
+    public void returnBook(Borrower borrower, Book book) {
+        if (borrower.returnBook(book)) {
+            books.add(book);
+            System.out.println(borrower.getName() + " returned: " + book.getTitle());
+        } else {
+            System.out.println(borrower.getName() + " did not borrow: " + book.getTitle());
+        }
+    }
+
+    public void displayLibraryBooks() {
+        System.out.println("Books in Library:");
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+        } else {
+            for (Book book : books) {
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("ISBN: " + book.getIsbn());
+                System.out.println("Author: " + book.getAuthor().getName());
+                System.out.println("Biography: " + book.getAuthor().getBio());
+                System.out.println("------------------------");
+            }
+        }
+    }
+}
+
+public class Main {
+    public Main() {
+        Author author1 = new Author("James Gosling", "Creator of the Java programming language.");
+        Author author2 = new Author("Robert C. Martin", "Known for his works on software craftsmanship.");
+
+        Book book1 = new Book("Java Programming", "123456789", author1);
+        Book book2 = new Book("Clean Code", "987654321", author2);
+
+        Library library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
+
+        Borrower borrower = new Borrower("Alice");
+        library.borrowBook(borrower, book1);
+        library.borrowBook(borrower, book2);
+
+        borrower.displayBorrowedBooks();
+
+        library.returnBook(borrower, book1);
+        borrower.displayBorrowedBooks();
+
+        library.displayLibraryBooks();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
