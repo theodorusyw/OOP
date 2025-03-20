24f7bd8baebee31d2c090a26b5a48d3ff2f28760
theodorusyw <theo.yuriputra19@gmail.com>
2025-03-20 22:54:06 +0700

    #

--- Hospital/hospital/Hospital.java
+++
@@ -1,5 +0,0 @@
-package Hospital;
-
-public class Hospital {
-    
-}
--- Hospital/main/Main.java
+++
@@ -1,9 +0,0 @@
-import hospital.Hospital;
-import hospital.Department;
-import people.Doctor;
-import people.Patient;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
