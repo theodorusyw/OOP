5ad3df6a5d1862dcf8c00435e358a8955bea5325
theodorusyw <theo.yuriputra19@gmail.com>
2025-05-16 14:38:40 +0700

    3

--- ClassManagement/src/uni/Main.java
+++ ClassManagement/src/uni/Main.java
@@ -1,17 +1,47 @@
 package uni;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import actor.Student;
+import actor.Lecturer;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+import academic.Class;
+import academic.Course;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Student(nim, name, dob)
+		ArrayList<Student> students = new ArrayList<>();
+		students.add(new Student("2809312213", "David", "16-7-1999"));
+    students.add(new Student("2809333797", "Donne", "7-7-1999"));
+		
+		//Lecturer(lecturerCode, name, dob)
+		Lecturer lec1 = new Lecturer("D7732", "Shamuell", "9-9-1999");
+		
+		//Class(classCode, semester, year)
+		Class cl1 = new Class("11LE", 2, "2024/2025"); 
+		//Course(courseCode, courseName, credit)
+		Course co1 = new Course("COMP990323", "Object Oriented Programming", 2);
+		
+		cl1.addCourse(co1);
+		
+		//addStudent(Student)
+		cl1.addStudent(students.get(0));
+		cl1.addStudent(students.get(1));
+		//addLecturer(Lecturer)
+		cl1.addLecturer(lec1, co1);
+		
+		//attend(IAttendant)
+		cl1.attend(students.get(0));
+		cl1.attend(students.get(1));
+		//takeAttendance(IAttendee)
+		cl1.takeAttendance(lec1);
+		
+		cl1.showTodayAttendance();
+		
+		//showCredits(semester)
+		students.get(0).showCredits(2);
+		
+		//showCourse(semester)
+		lec1.showCourse(2);
+	}
 }
